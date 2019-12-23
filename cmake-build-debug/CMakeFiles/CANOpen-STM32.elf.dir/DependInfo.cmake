2f0c468be51cd722d74ffce058c183bb9059c7d0
FalconHeavy <0xacefab@gmail.com>
2019-12-24 00:16:39 +0800

    Create Project with Clion and CubeMX

---
+++ cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/DependInfo.cmake
@@ -0,0 +1,99 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/bochao/Desktop/Project/CANOpen-STM32/startup/startup_stm32f446xx.s" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/startup/startup_stm32f446xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/list.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/can.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/can.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/dma.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/dma.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/freertos.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/freertos.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/gpio.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/gpio.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/main.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/main.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/stm32f4xx_hal_msp.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/stm32f4xx_hal_timebase_tim.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/stm32f4xx_hal_timebase_tim.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/stm32f4xx_it.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/stm32f4xx_it.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/sys.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/sys.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/syscalls.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/syscalls.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/system_stm32f4xx.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/system_stm32f4xx.c.obj"
+  "/home/bochao/Desktop/Project/CANOpen-STM32/Src/usart.c" "/home/bochao/Desktop/Project/CANOpen-STM32/cmake-build-debug/CMakeFiles/CANOpen-STM32.elf.dir/Src/usart.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
